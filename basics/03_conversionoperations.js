47170c13e874255a70b798917960d62ee01bda55
Pradeep Jagtap <168101019+PradeepJJagtap@users.noreply.github.com>
2024-04-26 14:30:54 +0000

    git push

---
+++ basics/03_conversionoperations.js
@@ -0,0 +1,70 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "33" --> 33
+// "33abc" --> NaN
+// true--> 1; false -->0
+
+let isLoggedIn = 0
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+
+// 1 --> true; 0 --> false
+// ""--> false
+// "hitesh" --> true
+
+
+let someNumber  = 46
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+// ************************** Opeartions******************************
+
+let value = 10
+
+let negValue = -value
+
+console.log(negValue);
+
+//console.log("1" + 2);
+//console.log(2 + "2");
+console.log("2" + 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log(2%2);
+
+//console.log(true);  
+
+console.log(+true); 
+
+//console.log(true+); 
+
+let str1 = "Pradeep"
+
+let str2 = " Jagtap"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+let gameCounter = 100
+
+gameCounter++;
+
+console.log(gameCounter);
+
+//*****************Link to study//////////////////////////
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
